a633e7c637e99a533f10f2e84c19052f95109002
Vedic Partap <vedicpartap1999@gmail.com>
2019-11-09 17:46:30 +0530

    v_02

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'hiVedic',         # How you named your package folder (MyLib)
   packages = ['hiVedic'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Test package',   # Give a short description about your library
   author = 'Vedic Partap',                   # Type in your name
   author_email = 'me@vedicpartap.com',      # Type in your E-Mail
   url = 'https://github.com/vedic-partap/hiVedic',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/vedic-partap/hiVedic/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/vedic-partap/hiVedic/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['test', 'hello world', 'First'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
          
