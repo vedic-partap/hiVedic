9b3f5379b0015e037b8202dec719eebf2445dfdd
Vedic Partap <vedicpartap1999@gmail.com>
2019-11-09 17:44:44 +0530

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   download_url = 'https://github.com/vedic-partap/hiVedic/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['test', 'hello world', 'First'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'getpass'
+         
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
